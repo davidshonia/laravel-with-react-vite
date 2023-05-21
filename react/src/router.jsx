63665f0788343a3eecc4953489e01edfce614644
David <dupecube@gmail.com>
2023-05-21 20:02:06 +0400

    created default and guest user components, added dashboard for authenticated users

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,20 +3,39 @@ import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Users from "./views/Users.jsx";
 import NotFound from "./views/NotFound.jsx";
+import DefaultLayout from "../components/DefaultLayout.jsx";
+import GuestLayout from "../components/GuestLayout.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login></Login>
+        path: '/',
+        element: <DefaultLayout></DefaultLayout>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard></Dashboard>
+            },
+            {
+                path: '/users',
+                element: <Users></Users>
+            }
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup></Signup>
-    },
-    {
-        path: '/users',
-        element: <Users></Users>
+        path: '/',
+        element: <GuestLayout></GuestLayout>,
+        children: [
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
     },
     {
         path: '*',
