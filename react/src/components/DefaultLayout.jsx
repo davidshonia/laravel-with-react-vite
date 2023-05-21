ef5c393d4b43ca47e0d33763491e8bc723c3246c
David <dupecube@gmail.com>
2023-05-21 20:24:51 +0400

    protecting routes with contextprovider for : authorized and guest users

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,16 @@
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import Login from "../views/Login.jsx";
 
 export default function DefaultLayout()
 {
+    const {user, token} = useStateContext()
+
+    if(!token)
+    {
+        return <Navigate to="/login"/>
+    }
+
     return (
         <div>
             <Outlet/>
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./router.jsx";
+import {ContextPrivder} from "./contexts/ContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ContextPrivder>
+            <RouterProvider router={router}/>
+        </ContextPrivder>
+    </React.StrictMode>,
 )
